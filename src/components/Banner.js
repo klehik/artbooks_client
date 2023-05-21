dabc840581b03d03729013266e463a2ab71aa714
AA4085 <aa4085@student.jamk.fi>
2023-05-21 10:45:07 +0300

    banner lorem ipsum removed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -45,19 +45,6 @@ const Banner = ({ header, paragraph }) => {
             <Typography align="center" style={{ color: 'white' }} variant="h3">
               {header}
             </Typography>
-            <Typography
-              align="center"
-              style={{ color: 'white', width: 120 }}
-              variant="p"
-            >
-              Impression, Sunrise became the most famous in the series after
-              being debuted in April 1874 in Paris at an exhibition by the group
-              "Painters, Sculptors, Engravers etc. Inc. Among thirty
-              participants, the exhibition was led by Monet, Edgar Degas,
-              Camille Pissarro, Pierre-Auguste Renoir, and Alfred Sisley, and
-              showed over two hundred works that were seen by about 4,000
-              people, including some rather unsympathetic critics.
-            </Typography>
           </Box>
         </Box>
       </Box>
